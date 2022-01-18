b9df780da65776012f5fee1851c0aa991885c968
Peter Damián Gómez <peterdamiang@gmail.com>
2022-01-18 16:16:50 -0300

    make: background.js
    only show plugin if we are in right place

--- background.js
+++ background.js
@@ -1,6 +1,14 @@
-let color = '#3aa757';
-
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostEquals: 'cloud.boosteroid.com' }
+          })
+        ],
+        actions: [new chrome.declarativeContent.ShowPageAction()]
+      }
+    ]);
+  });
 });
