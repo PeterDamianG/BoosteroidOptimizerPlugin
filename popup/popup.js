ce4cb5384a2f28d1a0364c73e2a2c10ff06f5631
Peter Damián Gómez <peterdamiang@gmail.com>
2022-02-16 04:45:27 -0300

    fix: add functional popup.js.

--- popup/popup.js
+++ popup/popup.js
@@ -1,7 +1,9 @@
 // Find switch element in popup.html
 const sw = document.querySelector('#noFullScreenSwitch');
 // Set actual state to switch.
-sw.checked = chrome.storage.sync.get('noFullScreen');
+chrome.storage.sync.get('noFullScreen', ({ noFullScreen }) => {
+  sw.checked = noFullScreen;
+});
 // Add event to change storage.
 sw.addEventListener('change', () =>
   chrome.storage.sync.set({ noFullScreen: sw.checked })
